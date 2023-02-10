6f7c9ffd6cfdde876915acb09913740544037ad0
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-09 20:37:55 -0800

    Add basic bootsrap components mimicing example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Image,
+  Navbar,
+  Button,
+  Card,
+  Container,
+  Col,
+  Row,
+} from "react-bootstrap";
+import SmolQuants_Logo from "./assets/images/smolquants-logo.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Image src={SmolQuants_Logo} height={50}></Image>
+        <Navbar.Text>SmolQuants</Navbar.Text>
+      </Container>
+      <Container>
+        <Card>
+          <Card.Title>Economic Risk Audits for DeFi Protocols</Card.Title>
+          <Card.Text>
+            Quickly and easily get started with Bootstrap's compiled,
+            production-ready files with this barebones example featuring some
+            basic HTML and helpful links. Download all our examples to get
+            started.
+          </Card.Text>
+          <Button>Schedule Audit</Button>
+        </Card>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <Card.Title>Start Projects</Card.Title>
+            <Card.Text>
+              Ready to beyond the starter template? Check out these open source
+              projects that you can quickly duplicate to a new GitHub
+              repository.
+            </Card.Text>
+            <Card.Link>Bootstrap npm starter</Card.Link>
+            <Card.Link>Bootstrap Parcel starter (coming soon!)</Card.Link>
+          </Col>
+          <Col>
+            <Card.Title>Guides</Card.Title>
+            <Card.Text>
+              Read more detailed instructions and documentation on using or
+              contributing to Bootstrap.
+            </Card.Text>
+            <Card.Link>Bootstrap quick start guide</Card.Link>
+            <Card.Link>Bootstrap Webpack guide</Card.Link>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
