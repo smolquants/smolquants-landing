950b28a8dfc92df7912a022a941ddfc526d7ae09
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-10 15:07:22 -0800

    Update headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { Image, Button, Card, Container, Col, Stack } from "react-bootstrap";
 import SmolQuants_Logo from "./assets/images/smolquants-logo.jpg";
+import styled from "styled-components";
 import "./App.css";
+
+const AppContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+`;
 
 interface SmolQuantsWorkAsset {
   description: string;
@@ -28,73 +35,83 @@ const smolQuantsWork: Array<SmolQuantsWorkAsset> = [
 
 function App() {
   return (
-    <div className="col-lg-8 mx-auto p-3 py-md-5">
-      <Stack gap={5}>
-        <Container fluid="md" style={{ maxWidth: "40rem" }}>
-          <Stack direction="horizontal" gap={3}>
-            <Image src={SmolQuants_Logo} height={60}></Image>
-            <div style={{ fontSize: "32px" }}>SmolQuants</div>
-          </Stack>
-        </Container>
+    <AppContainer>
+      <div className="col-lg-8 mx-auto p-3 py-md-5 align-middle">
+        <Stack gap={5}>
+          <Container
+            className="mb-3 mt-5"
+            fluid="md"
+            style={{ maxWidth: "40rem" }}
+          >
+            <Stack direction="horizontal" gap={3}>
+              <Image src={SmolQuants_Logo} height={60}></Image>
+              <h2>SmolQuants</h2>
+            </Stack>
+          </Container>
 
-        <Container fluid="md" style={{ maxWidth: "40rem" }}>
-          <Card style={{ border: "none", maxWidth: "40rem" }}>
-            <h5>
-              <b>Economic Risk Audits for DeFi Protocols</b>
-            </h5>
-            <Card.Text>
-              SmolQuants is a collective of DeFi Risk researchers. We provide
-              economic risk consulting & auditing services to various DeFi
-              protocols. Beyond audit and advisory services, we also build
-              open-source research tools that will support builders in making
-              DeFi safer for all participants.
-            </Card.Text>
-            <Button
-              href="https://airtable.com/shrqhCDLlaL784GGa"
-              target="_blank"
-              size="lg"
-              style={{ maxWidth: "20rem" }}
-            >
-              Contact Us
-            </Button>
-          </Card>
-        </Container>
+          <Container className="mb-4" fluid="md" style={{ maxWidth: "40rem" }}>
+            <Card style={{ border: "none", maxWidth: "40rem" }}>
+              <h5>
+                <b>Economic Risk Audits for DeFi Protocols</b>
+              </h5>
+              <Card.Text>
+                SmolQuants is a collective of DeFi Risk researchers. We provide
+                economic risk consulting & auditing services to various DeFi
+                protocols. Beyond audit and advisory services, we also build
+                open-source research tools that will support builders in making
+                DeFi safer for all participants.
+              </Card.Text>
+              <Button
+                href="https://airtable.com/shrqhCDLlaL784GGa"
+                target="_blank"
+                size="lg"
+                style={{ maxWidth: "20rem" }}
+              >
+                Contact Us
+              </Button>
+            </Card>
+          </Container>
 
-        <Container fluid="md" style={{ maxWidth: "40rem" }}>
-          <div className="row g-5">
-            <Col md={6}>
-              <Stack gap={2}>
-                <h6>Links</h6>
-                <a
-                  href="https://smolquants.gitbook.io/about-us/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  About Us
-                </a>
-                <a
-                  href="https://github.com/smolquants"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Github
-                </a>
-              </Stack>
-            </Col>
-            <Col md={6}>
-              <Stack gap={2}>
-                <h6>Our Work</h6>
-                {smolQuantsWork.map((asset, index) => (
-                  <a href={asset.link} target="_blank" rel="noreferrer">
-                    {asset.description}
+          <Container className="mb-5" fluid="md" style={{ maxWidth: "40rem" }}>
+            <div className="row g-5">
+              <Col md={6}>
+                <h5>
+                  <b>Links</b>
+                </h5>
+                <Stack gap={2}>
+                  <a
+                    href="https://smolquants.gitbook.io/about-us/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    About Us
                   </a>
-                ))}
-              </Stack>
-            </Col>
-          </div>
-        </Container>
-      </Stack>
-    </div>
+                  <a
+                    href="https://github.com/smolquants"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Github
+                  </a>
+                </Stack>
+              </Col>
+              <Col md={6}>
+                <h5>
+                  <b>Our Work</b>
+                </h5>
+                <Stack gap={2}>
+                  {smolQuantsWork.map((asset, index) => (
+                    <a href={asset.link} target="_blank" rel="noreferrer">
+                      {asset.description}
+                    </a>
+                  ))}
+                </Stack>
+              </Col>
+            </div>
+          </Container>
+        </Stack>
+      </div>
+    </AppContainer>
   );
 }
 
