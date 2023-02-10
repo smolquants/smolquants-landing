bc7e37075bb0b5fc84414ac33a14dc6bda19a845
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-02-09 22:11:53 -0800

    Style

--- src/App.tsx
+++ src/App.tsx
@@ -9,38 +9,48 @@ import {
   Row,
   Stack,
 } from "react-bootstrap";
+import styled from "styled-components";
 import SmolQuants_Logo from "./assets/images/smolquants-logo.jpg";
 import "./App.css";
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+`;
+
 function App() {
   return (
-    <div>
-      <Container>
-        <Image src={SmolQuants_Logo} height={50}></Image>
-        <Navbar.Text>SmolQuants</Navbar.Text>
+    <AppContainer>
+      <Container style={{ marginBottom: "1.5rem", width: "40rem" }}>
+        <Stack direction="horizontal" gap={3}>
+          <Image src={SmolQuants_Logo} height={60}></Image>
+          <Card.Title style={{ fontSize: "32px" }}>SmolQuants</Card.Title>
+        </Stack>
       </Container>
-      <Container>
-        <Card style={{ border: "none", margin: "16px 0" }}>
-          <Card.Title style={{ width: "24rem" }}>
-            Economic Risk Audits for DeFi Protocols
-          </Card.Title>
-          <Card.Text style={{ width: "40rem" }}>
+
+      <Container style={{ marginBottom: "2rem", width: "40rem" }}>
+        <Card style={{ border: "none", margin: "16px 0", width: "40rem" }}>
+          <Card.Title>Economic Risk Audits for DeFi Protocols</Card.Title>
+          <Card.Text>
             SmolQuants is a collective of DeFi Risk experts. We provide economic
             risk consulting & auditing services to various DeFi protocols.
             Beyond audit and advisory services, we also build open-source
             research tools that will support builders in making DeFi safer for
             all participants.
           </Card.Text>
-          <div>
-            <Button size="lg">Schedule Audit</Button>
-          </div>
+          <Button size="lg" style={{ width: "20rem" }}>
+            Schedule Audit
+          </Button>
         </Card>
       </Container>
-      <Container>
+
+      <Container style={{ marginBottom: "2rem", width: "40rem" }}>
         <Row>
           <Col xs={6}>
-            <Card.Title>Links</Card.Title>
-            <Stack>
+            <Stack gap={2}>
+              <Card.Title>Links</Card.Title>
               <Card.Link href="https://app.gitbook.com/o/pjyIvdmFsgl3q0dQOa9T/s/jMHEocVZwQUdszK9KkFQ/">
                 Documentation
               </Card.Link>
@@ -48,8 +58,8 @@ function App() {
             </Stack>
           </Col>
           <Col>
-            <Card.Title>Past Work</Card.Title>
-            <Stack>
+            <Stack gap={2}>
+              <Card.Title>Past Work</Card.Title>
               <Card.Link href="https://github.com/smolquants/y2k-audit-2022/blob/main/audit/audit.pdf">
                 Y2K
               </Card.Link>
@@ -57,7 +67,7 @@ function App() {
           </Col>
         </Row>
       </Container>
-    </div>
+    </AppContainer>
   );
 }
 
